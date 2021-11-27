a2134aa4067ae41487dff60c7f5124d922b681cc
Rashmi Chhabria <resh.10chh@gmail.com>
2021-11-27 17:33:04 -0600

    Appending datasets for tornadoes, hurricanes, wildfires and GDP

--- Natural_Hazards_and_its_Economic_Impacts_in_the_US.py
+++ Natural_Hazards_and_its_Economic_Impacts_in_the_US.py
@@ -27,7 +27,11 @@ import csv
 if __name__ == '__main__':
     earthquake_file = open("data/earthquakes.tsv")
     tsunamis_file = open('data/tsunamis.tsv')
-    volcanoes_file = open('data/volcano.tsv')
+    volcanoes_file = open('data/volcanoes.tsv')
+    tornadoes_data = open('data/tornadoes.csv')
+    hurricanes_data = open('data/hurricanes.csv')
+    wildfires_data = open('data/wildfires.csv')
+    GDP_by_state_data = open('data/GDP_by_state.csv')
     earthquake_data = csv.reader(earthquake_file, delimiter="\t")
     tsunamis_data = csv.reader(tsunamis_file, delimiter="\t")
     volcanoes_data = csv.reader(volcanoes_file, delimiter="\t")
